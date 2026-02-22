e90e27fef5a30c84d29a0172c7088659a0eb9aed
TegiriAshwini <161735045+TegiriAshwini@users.noreply.github.com>
2026-02-22 18:56:48 +0530

    Add full adder module implementation

---
+++ halfadder_and_fulladder/full_adder.v
@@ -0,0 +1,14 @@
+//sum= a xor b xor cin
+//carry = a*b +(a xor b)cin
+module full_adder(
+input a,b,cin,
+output sum, carry
+);
+wire w1,w2,w3;
+xor(w1,a,b);
+xor(sum,w1,cin);
+
+and(w2,a,b);
+and(w3,w1,cin);
+or(carry,w2,w3);
+endmodule
