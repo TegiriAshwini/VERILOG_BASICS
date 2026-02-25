3fab4ba4b27e2299e5879f9c685284233f6402c3
TegiriAshwini <161735045+TegiriAshwini@users.noreply.github.com>
2026-02-25 19:48:36 +0530

    Add testbench for ripple carry adder

---
+++ ripple_carry_adder/tb_ripple_carry_adder.v
@@ -0,0 +1,14 @@
+module tb_ripple_carry_adder;
+reg [3:0]a,b;
+reg cin;
+wire [3:0] sum;
+wire cout;
+ripple_carry_adder_4bit uut(a,b,cin,sum,cout);
+initial begin
+a=4'b0000;b=4'b0000;cin=0;#10;
+a=4'b1111;b=4'b0001;cin=0;#10;
+a=4'b0101;b=4'b0011;cin=0;#10;
+a=4'b1010;b=4'b0101;cin=0;#10;
+$finish;
+end 
+endmodule
