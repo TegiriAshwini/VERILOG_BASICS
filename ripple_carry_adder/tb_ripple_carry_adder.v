b6d0633e80bd4848b1a5b67e2876838a999b431c
TegiriAshwini <161735045+TegiriAshwini@users.noreply.github.com>
2026-02-28 08:31:03 +0530

    Update tb_ripple_carry_adder.v

--- ripple_carry_adder/tb_ripple_carry_adder.v
+++ ripple_carry_adder/tb_ripple_carry_adder.v
@@ -5,10 +5,16 @@ wire [3:0] sum;
 wire cout;
 ripple_carry_adder_4bit uut(a,b,cin,sum,cout);
 initial begin
-a=4'b0000;b=4'b0000;cin=0;#10;
-a=4'b1111;b=4'b0001;cin=0;#10;
-a=4'b0101;b=4'b0011;cin=0;#10;
-a=4'b1010;b=4'b0101;cin=0;#10;
+a=4'b0000; b=4'b0000; cin=0; #10;
+a=4'b0101; b=4'b0011; cin=0; #10;
+a=4'b1111; b=4'b0001; cin=0; #10;
+a=4'b1010; b=4'b0101; cin=0; #10;
+
+a=4'b0000; b=4'b0000; cin=1; #10;
+a=4'b0101; b=4'b0011; cin=1; #10;
+a=4'b1110; b=4'b0001; cin=1; #10;
+a=4'b1111; b=4'b1111; cin=1; #10;
+
 $finish;
 end 
 endmodule
